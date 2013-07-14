d10b5d42c716bc71c05eaa0c458af47adfaf1847
Nik Molnar <nikmd23@gmail.com>
2013-07-14 10:51:41 -0400

    Cleaned up using statements

--- source/Signatory/Models/GitHubService.cs
+++ source/Signatory/Models/GitHubService.cs
@@ -1,12 +1,8 @@
 ﻿using Newtonsoft.Json;
 using Signatory.Extensions;
-using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Web;
 
 namespace Signatory.Models
 {
